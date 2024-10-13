0d21e69d0f1214ef1fe30e07b2d4fdbd88edc579
Mahesh kumar <mahesh.mahi24102004@gmail.com>
2024-10-13 17:52:39 +0530

    stack data-structure

---
+++ ValidParenthesis.java
@@ -0,0 +1,49 @@
+package leet_code;
+import java.util.Stack;
+
+// Function to check if brackets are balanced
+public class ValidParenthesis {
+    public static boolean ispar(String s) {  
+        // Declare a stack to hold the previous brackets.
+        Stack<Character> stk = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+          
+            // Check if the character is an opening bracket
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                stk.push(s.charAt(i)); 
+            } 
+            else {
+              
+                // If it's a closing bracket, check if the stack is non-empty
+                // and if the top of the stack is a matching opening bracket
+                if (!stk.empty() && 
+                    ((stk.peek() == '(' && s.charAt(i) == ')') ||
+                     (stk.peek() == '{' && s.charAt(i) == '}') ||
+                     (stk.peek() == '[' && s.charAt(i) == ']'))) {
+                    stk.pop(); 
+                }
+                else {
+                    return false; // Unmatched closing bracket
+                }
+            }
+        }
+      
+        // If stack is empty, return true (balanced), 
+        // otherwise false
+        return stk.empty();
+    }
+
+    public static void main(String[] args) {
+        String s = "{()}[]";
+        if (ispar(s))
+            System.out.println("true");
+        else
+            System.out.println("false");
+
+            String s1 = "{([])()()})[]";
+        if (ispar(s1))
+            System.out.println("true");
+        else
+            System.out.println("false");
+    }
+}
